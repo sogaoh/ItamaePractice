82d8b5282bc203fab586007f7fc2bf2c2102716b
sogaoh <sogaoh@outlook.com>
2016-12-31 22:47:04 +0900

    correct jenkins install process 3

--- cookbooks/jenkins/default.rb
+++ cookbooks/jenkins/default.rb
@@ -6,11 +6,13 @@ end
 
 execute "download repo" do
   command "wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo"
+  user 'root'
   not_if "test -e /etc/yum.repos.d/jenkins.repo"
 end
 
 execute "import jenkins key" do
   command "rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key"
+  user 'root'
 end
 
 package "jenkins" do
