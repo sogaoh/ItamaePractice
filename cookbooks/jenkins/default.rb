c6078312927de2c82d1182f9feaacced66c3591e
sogaoh <sogaoh@outlook.com>
2016-12-31 22:32:49 +0900

    correct jenkins install process 1

--- cookbooks/jenkins/default.rb
+++ cookbooks/jenkins/default.rb
@@ -1,8 +1,16 @@
 %w{
   java-1.8.0-openjdk
-  jenkins
 }.each do |pkg|
   package pkg
+end
+
+execute 'install jenkins' do
+  command <<-EOL
+    wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-$ stable/jenkins.repo
+    rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+    yum install jenkins
+  EOL
+  user 'root'
 end
 
 service "jenkins" do
